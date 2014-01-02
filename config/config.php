2e34ac19e850cb70c2f05421138517d785cfaf06
Fabien INIZAN <fabien.inizan@gmail.com>
2014-01-02 16:09:27 +0100

    Added a quick start guide in the README

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+	/* Database configuration */
+	$conf['dbType'] = 'mysql';
+	$conf['dbName'] = 'test';
+	$conf['dbHost'] = 'localhost';
+	$conf['dbUser'] = 'root';
+	$conf['dbPassword'] = 'paradox66';
+
+	/* Cryptography configuration */
+	$conf['cryptoSalt'] = 'webKernelSaltedCrypt';
+
+?>
--- plugins/page/__mkdir.php
+++
@@ -1,7 +0,0 @@
-<?php
-	return array(
-		'modules'=>'page',
-		'templates/admin'=>'page',
-		'templates/public'=>'page'
-	);
-?>
